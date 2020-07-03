e2467ea7ea906566e0c2c49960eb2a38113ccffe
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2020-07-02 21:52:44 -0500

    new files after solving conflicts

--- sources/build.sh
+++ sources/build.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # source venv/bin/activate
 set -e
 
@@ -45,20 +44,3 @@ done
 
 rm -rf master_ufo/ instance_ufo/
 
-=======
-#!/bin/sh
-set -e
-
-rm -rf ../fonts
-mkdir -p ../fonts/{ttf,otf}
-glyphs=$(ls *.glyphs)
-
-echo "Generating static fonts"
-for glyph in $glyphs
-do
-	fontmake -g $glyph -o variable -i -o ttf --output-dir ../fonts/ttf/
-	fontmake -g $glyph -o variable -i -o otf --output-dir ../fonts/otf/
-done
-
-rm -rf master_ufo/ instance_ufo/
->>>>>>> 35c9ddf877c7ed839191ae148aa2038b941ae1a3
